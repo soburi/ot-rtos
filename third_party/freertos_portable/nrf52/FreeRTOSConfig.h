a4b73509b09aaadc0fc6b55a46e10fb4185dd3cc
soburi <soburi@gmail.com>
2019-08-17 21:36:10 +0900

    add option

--- third_party/freertos_portable/nrf52/FreeRTOSConfig.h
+++ third_party/freertos_portable/nrf52/FreeRTOSConfig.h
@@ -75,6 +75,8 @@
 #define configUSE_NEWLIB_REENTRANT                               0
 #define configENABLE_BACKWARD_COMPATIBILITY                      1
 
+#define configSUPPORT_STATIC_ALLOCATION                          1
+#define configSUPPORT_DYNAMIC_ALLOCATION                         1
 
 /* Hook function related definitions. */
 #define configUSE_IDLE_HOOK                                      0
